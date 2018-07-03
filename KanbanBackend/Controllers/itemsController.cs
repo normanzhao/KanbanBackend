0a83dffa5fe0f7ce5a41ced2c9b8d5a9da5dcda6
Norman Zhao <normanlz.zhao@gmail.com>
2018-07-02 23:20:15 -0400

    Initial commit of working backend with database set up in .mdf format. Output is still somewhat raw and needs to be fixed to return elevant data.

---
+++ KanbanBackend/Controllers/itemsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using KanbanBackend.Models;
+
+namespace KanbanBackend.Controllers
+{
+    public class itemsController : ApiController
+    {
+        private KanbanDBEntities db = new KanbanDBEntities();
+
+        // GET: api/items
+        public IQueryable<item> Getitems()
+        {
+            return db.items.ToList()
+            .Select(i => new item
+            {
+                id = i.id,
+                p_id = i.p_id,
+                type = i.type,
+                priority = i.priority,
+                title = i.title,
+                description = i.description
+            }).AsQueryable();
+        }
+
+        // GET: api/items/5
+        [ResponseType(typeof(item))]
+        public async Task<IHttpActionResult> Getitem(int id)
+        {
+            item item = await db.items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        // PUT: api/items/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> Putitem(int id, item item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != item.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(item).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!itemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/items
+        [ResponseType(typeof(item))]
+        public async Task<IHttpActionResult> Postitem(item item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.items.Add(item);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = item.id }, item);
+        }
+
+        // DELETE: api/items/5
+        [ResponseType(typeof(item))]
+        public async Task<IHttpActionResult> Deleteitem(int id)
+        {
+            item item = await db.items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            db.items.Remove(item);
+            await db.SaveChangesAsync();
+
+            return Ok(item);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool itemExists(int id)
+        {
+            return db.items.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ KanbanBackend/Controllers/projectsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KanbanBackend.Models;
+
+namespace KanbanBackend.Controllers
+{
+    public class projectsController : Controller
+    {
+        private KanbanDBEntities db = new KanbanDBEntities();
+
+        // GET: projects
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.projects.ToListAsync());
+        }
+
+        // GET: projects/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            project project = await db.projects.FindAsync(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+
+        // GET: projects/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: projects/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "id,title,acronym,description")] project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.projects.Add(project);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(project);
+        }
+
+        // GET: projects/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            project project = await db.projects.FindAsync(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+
+        // POST: projects/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id,title,acronym,description")] project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(project).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(project);
+        }
+
+        // GET: projects/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            project project = await db.projects.FindAsync(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+
+        // POST: projects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            project project = await db.projects.FindAsync(id);
+            db.projects.Remove(project);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
